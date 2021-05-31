aeb786fc7882f60b34d655884daab8b5b41c5526
vignesh1992 <ti.vignesh@gmail.com>
2021-05-31 11:29:08 +0200

    Initial commit

---
+++ expense-manager-management/get-expense-categories.js
@@ -0,0 +1,11 @@
+exports.handler = async (event) => {
+  
+  let response = {
+      statusCode: 200,
+      body: ["Groceries", "Shopping"]
+  };
+
+  console.log("Returning expense category labels")
+  
+  return response;
+};
