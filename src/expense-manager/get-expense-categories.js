f332ddc5c3d768977d78a65faf977127755f89d6
vignesh1992 <ti.vignesh@gmail.com>
2021-05-31 14:56:00 +0200

    Update get expense category lambda

--- src/expense-manager/get-expense-categories.js
+++ src/expense-manager/get-expense-categories.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   
   let response = {
       statusCode: 200,
-      body: ["Groceries", "Shopping"]
+      body: ["Groceries", "Shopping", "Rent"]
   };
 
   console.log("Returning expense category labels")
