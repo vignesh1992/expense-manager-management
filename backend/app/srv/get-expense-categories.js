5de5d0354a38c02995ca7e03469ce576bc25fd12
vignesh1992 <ti.vignesh@gmail.com>
2021-06-06 12:45:56 +0200

    Change project structure and update buildspec

--- backend/app/srv/get-expense-categories.js
+++ backend/app/srv/get-expense-categories.js
@@ -1,8 +1,9 @@
 exports.handler = async (event) => {
   
+  let expenseCategories = ["Groceries", "Shopping", "Housing", "Financial Expenses"];
   let response = {
       statusCode: 200,
-      body: ["Groceries", "Shopping", "Housing"]
+      body: JSON.stringify(expenseCategories)
   };
 
   console.log("Returning expense category labels")
